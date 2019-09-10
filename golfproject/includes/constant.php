55f91c140cbf8619a1a4bdebd19a430ff0fb7ad2
Anup Gc <gclncguy@gmail.com>
2019-09-10 13:06:52 +0300

    Minor changes to links

--- golfproject/includes/constant.php
+++ golfproject/includes/constant.php
@@ -4,11 +4,11 @@
 	define('USERNAME','linkinfo_system');
 	define('PASSWORD','manager');
 	define('DATABASE_NAME','linkinfo_mydb');
-	define('SITE_URL','http://www.link-infosys.com/golfproject/'); // url of our site
+	define('SITE_URL','http://www.anupgc.com/golfproject/'); // url of our site
 	define('SITE_PATH',$_SERVER['DOCUMENT_ROOT']."/"); // path of our site
-	define('SITE_ADMIN_URL','http://www.link-infosys.com/golfproject/admin'); 
+	define('SITE_ADMIN_URL','http://www.anupgc.com/golfproject/admin'); 
 	define('SITE_ADMIN_PATH',$_SERVER['DOCUMENT_ROOT']."/admin/"); 
-	define('SITE_FRONT_URL','http://www.link-infosys.com/golfproject/'); 
+	define('SITE_FRONT_URL','http://www.anupgc.com/golfproject/'); 
 	define('SITE_FRONT_PATH',$_SERVER['DOCUMENT_ROOT']."/"); 
 	define('GALLERY_PATH',$_SERVER['DOCUMENT_ROOT']."/gallery/"); 
 	define('GALLERY_URL',SITE_URL."gallery/"); 
